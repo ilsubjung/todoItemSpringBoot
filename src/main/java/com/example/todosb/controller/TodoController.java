91d6bc4b02efcbde1760965fddfd477eaa6c3335
ilsubjung1 <ilsubjung1@nextgen.com>
2023-12-11 14:17:04 -0500

    delete added

--- src/main/java/com/example/todosb/controller/TodoController.java
+++ src/main/java/com/example/todosb/controller/TodoController.java
@@ -6,6 +6,7 @@ import com.example.todosb.repository.TodoItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +15,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
@@ -45,7 +48,20 @@ public class TodoController {
     }
 
     @PutMapping("/todoItem/{id}")
-    public ResponseEntity<TodoItem> updateTodoItem(@PathVariable Long id, @RequestBody TodoItem todoItem) {
-        todoItemRepository.save()
+    public ResponseEntity<TodoItem> updateTodoItem(@PathVariable Long id, @RequestBody TodoItem newTodoItem) {
+        TodoItem todoItem = todoItemRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Todoitem not found with id ="+id));
+        todoItem.setName(newTodoItem.getName());
+        todoItem.setStatus(newTodoItem.getStatus());
+        TodoItem updatedItem = todoItemRepository.save(todoItem);
+        return ResponseEntity.ok(updatedItem);
+    }
+
+    @DeleteMapping("/todoItem/{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteTodoItem(@PathVariable Long id) {
+        TodoItem todoItem = todoItemRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Todoitem not found with id ="+id));
+        todoItemRepository.delete(todoItem);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted",Boolean.TRUE);
+        return ResponseEntity.ok(response);
     }
 }
